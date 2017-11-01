fade75745992ad5b6f3389a2f77ca82371d7e849
Kyle Geske <stungeye@gmail.com>
2017-11-01 13:16:56 +0000

    Added an image column to students.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030163956) do
+ActiveRecord::Schema.define(version: 20171101131515) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171030163956) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "school_id"
+    t.string "image"
     t.index ["school_id"], name: "index_students_on_school_id"
   end
 
